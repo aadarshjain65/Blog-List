9fd4e91e31da4e4afa51ed0b731db1053dbf6d7b
Aadarsh Jain <aadarshjain0605@gmail.com>
2024-02-05 06:23:25 +0000

    Blog List Solution

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+const BlogItem = props => {
+  const {blogDetails} = props
+  const {title, description, publishedDate} = blogDetails
+  return (
+    <li className="blog-item">
+      <div className="title-and-date-container">
+        <h1 className="title">{title}</h1>
+        <p className="date">{publishedDate}</p>
+      </div>
+      <p className="description">{description}</p>
+      <hr className="horizontal-line" />
+    </li>
+  )
+}
+
+export default BlogItem
